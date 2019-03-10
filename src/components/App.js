071ac2d3c6b759de1496bab78be85ba068ec2075
git <sygnowski.m@gmail.com>
2019-03-10 13:38:52 +0000

    testing 25

--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,7 @@ export default class App extends Component {
     // console.log(window.location.pathname);
 
     return (
-      <Router basename={"fads/"}>
+      <Router>
         <Fragment>
           <Header />
           <Switch>
